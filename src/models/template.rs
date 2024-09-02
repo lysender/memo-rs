b614157a0f4a553e037e6ffc8d9c33d0b799d917
Lysender <lysender@darkstar-win>
2024-09-02 13:01:15 +0800

    fix: #5 add optional googla analytics tag

--- src/models/template.rs
+++ src/models/template.rs
@@ -9,6 +9,7 @@ pub struct TemplateData {
     pub styles: Vec<String>,
     pub scripts: Vec<String>,
     pub async_scripts: Vec<String>,
+    pub ga_tag_id: Option<String>,
     pub actor: Option<Actor>,
 }
 
@@ -23,6 +24,7 @@ impl TemplateData {
             styles: Vec::new(),
             scripts: Vec::new(),
             async_scripts: Vec::new(),
+            ga_tag_id: config.ga_tag_id.clone(),
             actor,
         }
     }
