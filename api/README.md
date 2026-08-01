c6c3b87525f51d5a0cad26783eb2abb6171e13da
Lysender <lysender@darkstar>
2026-08-01 13:52:50 +0800

    fix: simplify notes

--- api/README.md
+++ api/README.md
@@ -97,4 +97,6 @@ Note:
 - id
 - file_id
 - content
+- revised_at (null: latest revision, not null: previous revision)
 - created_at
+
--- db/db/migrations/memo/11-create-notes.sql
+++ db/db/migrations/memo/11-create-notes.sql
@@ -2,8 +2,10 @@ CREATE TABLE notes (
     id TEXT PRIMARY KEY,
     file_id TEXT NOT NULL,
     content TEXT NOT NULL,
+    revised_at INTEGER NULL,
     created_at INTEGER NOT NULL,
     FOREIGN KEY (file_id) REFERENCES files(id)
 ) STRICT;
 
-CREATE INDEX idx_notes_file_id_id ON notes(file_id, id DESC);
+CREATE INDEX idx_notes_file_id ON notes(file_id);
+CREATE UNIQUE INDEX idx_notes_file_id_revised_at ON notes(file_id, revised_at);
